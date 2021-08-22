0c3f1a609b8a931dbdd9331dd224ec88c7bea73a
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-08-23 00:37:48 +0200

    initial Geo Service

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,9 @@
 import { GeolocalizationProviderService } from './../services/geolocalization-provider.service';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 import { BluetoothProviderService } from './../services/bluetooth-provider.service';
+import { BLE } from '@ionic-native/ble/ngx';
+import { BluetoothLE } from '@ionic-native/bluetooth-le/ngx';
 import { WifiProviderService } from './../services/wifi-provider.service';
 import { WifiWizard2 } from '@ionic-native/wifi-wizard-2/ngx';
 import { NgModule } from '@angular/core';
@@ -10,7 +14,6 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
-import { Geolocation } from '@ionic-native/geolocation/ngx';
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
   declarations: [HomePage],
@@ -18,6 +21,9 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
     WifiWizard2,
     WifiProviderService,
     BluetoothProviderService,
+    Geolocation,
+    BluetoothLE,
+    BLE,
     GeolocalizationProviderService,
   ],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { WifiProviderService } from './../services/wifi-provider.service';
 import { Component } from '@angular/core';
+import { BluetoothProviderService } from '../services/bluetooth-provider.service';
+import { GeolocalizationProviderService } from '../services/geolocalization-provider.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private wifiProvider: WifiProviderService) {}
+  constructor(
+    private wifiProvider: WifiProviderService,
+    private geoProvider: GeolocalizationProviderService,
+    private bleProvider: BluetoothProviderService
+  ) {}
 
   startService: boolean = false;
   results: string[] = [];
@@ -19,9 +25,21 @@ export class HomePage {
       await this.wifiProvider.getNetworks();
       this.interval = setInterval(async () => {
         const networks = await this.wifiProvider.getNetworks();
+        const geoCoord = await this.geoProvider.getGPSLocation();
+        const ble = await this.bleProvider.getBluetoothDevices();
         this.ok++;
         // console.log(networks);
-        this.results = networks.map((r) => `${r.BSSID} -> ${r.level}`);
+        this.results = networks.map(
+          (r) =>
+            `${r.BSSID} -> ${r.level}` +
+            ` Geo: { lat: ` +
+            geoCoord.coords.latitude +
+            ` lng: ` +
+            geoCoord.coords.longitude +
+            ` alt: ` +
+            geoCoord.coords.altitude +
+            ` }`
+        );
       }, 2100);
     } else clearInterval(this.interval);
   }
