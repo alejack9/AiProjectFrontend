04dbc1e9c53cdf2cb0fbcef7f15fb49bbfeaa3c6
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-09-05 18:42:26 +0200

    add ionic native http

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -15,6 +15,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @NgModule({
   imports: [
@@ -26,6 +27,7 @@ import { HomePageRoutingModule } from './home-routing.module';
   ],
   declarations: [HomePage],
   providers: [
+    HTTP,
     WifiProviderService,
     BluetoothProviderService,
     Geolocation,
